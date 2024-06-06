ecfcd54282aadc1a65945366b61270b27a3844e3
Trish Gill <95247759+sdtrish-dev@users.noreply.github.com>
2024-06-06 16:47:19 -0400

    [chore] update page controller and route

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -6,26 +6,6 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::page.page", ({ strapi }) => ({
-  async findByCategory(ctx) {
-    const { categorySlug } = ctx.params;
 
-    const category = await strapi.db.query("api::category.category").findOne({
-      where: { slug: categorySlug },
-      populate: { pages: true },
-    });
-
-    if (!category) {
-      return ctx.notFound("Category not found");
-    }
-
-    const pages = await strapi.db.query("api::page.page").findMany({
-      where: {
-        categories: category.id,
-      },
-    });
-
-    ctx.body = pages;
-  },
-}));
+module.exports = createCoreController("api::page.page");
 
